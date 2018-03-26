a41ee291d41d6e703f4052a9a9781de14c46a6af
jw66melo <jw66melo@studserv.uni-leipzig.de>
2018-03-26 22:27:57 +0200

    fix issues, readme update

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-# My Hammerspoon Config
+# Hammerspoon Config
 
 ## Modules
 
 ### Audio
 
-Just working with the browser "Safari".
+Working with Safari and Chrome
 
 * iTunes to Youtube-Video:`musicVideo.transitionfromiTunes`
 
-
 * Spotify to Youtube-Video:`musicVideo.transitionfromSpotify`
+
+
+---------------------------------------------------------------
+## How to install
+
+1. Download and install [hammerspoon](https://www.hammerspoon.org/).
+2. Insert your own api_key into the `api_key.lua`.
+3. Choose your own Hotkey or use by default `cmd` + `ctrl` + `s` .
+
+
+
+
+
+
+
--- init.lua
+++ init.lua
@@ -1,20 +1,32 @@
--- Spotify to YouTube Video
-
-
 -- load modules
 local youtube = require "youtube"
+local sockets = require "sockets"
 
 
-hs.hotkey.bind({"cmd", "ctrl"}, "y", function()
-    print('Hello')
-  youtube.transitionToVideo()
+
+-- hyper key binding using: https://github.com/tekezo/Karabiner-Elements
+local hyper = {"cmd", "alt"}
+
+-- hotkey bindings
+
+hs.hotkey.bind({"cmd", "ctrl"}, "s", function()
+	youtube.transitionToVideo()
 end)
 
---hs.hotkey.bind({"cmd", "ctrl"}, "x",function()
---print('Loading URL')
---state, result = hs.osascript.applescript("tell application \"Safari\" \n set theURL to URL of front document \n return theURL \n end tell")
---print(result)
---end)
+
+-- _wifi watcher
+wifiwatcher = hs.wifi.watcher.new(function ()
+	network = hs.wifi.currentNetwork()
+	if network==nil then
+		subTitle = 'Disconnected'
+	else
+		subTitle = 'Connected to ' .. network
+		--if network == 'eduroam' then initUni() end
+	end
+--	hs.notify.new({title        = 'Wi-Fi Status',subTitle     = subTitle,}):send()
+end)
+wifiwatcher:start()
+
 
 -- Reload config when any lua file in config directory changes, to save having to manually reload.
 function reloadConfig(files)
@@ -28,5 +40,4 @@ function reloadConfig(files)
         hs.reload()
     end
 end
-local myWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
-hs.timer.doAt("10:45", function() hs.spotify.play() end)
+
